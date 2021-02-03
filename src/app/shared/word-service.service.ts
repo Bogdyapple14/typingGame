541ea47ac033283fa5a1f7f660451efbca0e47fc
Bogdyapple14 <bogdybogdan34@gmail.com>
2021-02-03 15:37:23 +0200

    Fixed a bug

--- src/app/shared/word-service.service.ts
+++ src/app/shared/word-service.service.ts
@@ -1003,10 +1003,7 @@ export class WordServiceService {
     `yourself`,
   ];
   private texts = [
-    `Computer programming is the process of designing and building an executable computer program to accomplish a specific 
-    computing result or to perform a specific task. Programming involves tasks such as: analysis, generating algorithms, 
-    profiling algorithms' accuracy and resource consumption, and the implementation of algorithms in a chosen programming
-     language (commonly referred to as coding).`,
+    `Computer programming is the process of designing and building an executable computer program to accomplish a specific computing result or to perform a specific task. Programming involves tasks such as: analysis, generating algorithms, profiling algorithms' accuracy and resource consumption, and the implementation of algorithms in a chosen programming language (commonly referred to as coding).`,
   ];
   private scores: Score[];
 
